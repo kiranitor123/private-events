4ad915ed6d7384b0f51becf346b747cfe79344a7
kiranitor123 <rolandoalvarezfaye@gmail.com>
2021-04-29 13:23:38 -0400

    updates user model routes update navbar add new session

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :events, inverse_of: :creator
-  has-many :invitations
+  has_many :invitations
   has_many :attended_events, through: :invitations, source: :event
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/index'
-  get 'invitations/sessions'
-  get 'users/index'
-  resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'events#index'
+
+  get 'signup' => 'users#new', as: 'signup'
+  get 'login' => 'sessions#new', as: 'login'
+  get 'logout' => 'sessions#destroy', as: 'logout'
+
+  resources :users
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  resources :events
+
+  resources :invitations, only: [:create, :destroy, :update]
 end
