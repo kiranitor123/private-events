27f1676c140b95b4a6a6f89f91252f8cdacd398a
German Renato Cobian <germancobian@hotmail.com>
2021-04-29 09:09:08 -0700

    Update models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ApplicationRecord
-  belongs_to :user
+  belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+  has_many :invitations
+  has_many :attendess, through: :invitations, source: :user
+  
+  scope :past, -> { where('date < ?', Date.today) }
+  scope :upcoming, -> { where('date >= ?', Date.today) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :events, inverse_of: :creator
+  has-many :invitations
+  has_many :attended_events, through: :invitations, source: :event
 end
