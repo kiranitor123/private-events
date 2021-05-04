5b4a8293541fa60a82eb068fdff9e4718650f983
German Renato Cobian <germancobian@hotmail.com>
2021-05-04 13:00:55 -0700

    Resolve linter errors

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -20,11 +20,10 @@ class InvitationsController < ApplicationController
     event = Event.find(params[:event_id])
     user = User.find(params[:user_id])
     @invitation = Invitation.new(event_id: event.id, user_id: host.id, invitee_id: user.id)
-    if @invitation.save
+    return unless @invitation.save
       respond_to do |format|
         format.html { redirect_to event }
         format.js
-      end
     end
   end
 
