d68ead2606d8d242431e8d49480efe99d7fd4e6b
kiranitor123 <rolandoalvarezfaye@gmail.com>
2021-05-05 11:06:58 -0400

    linter error

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,8 +2,8 @@ class Invitation < ApplicationRecord
   belongs_to :user, class_name: 'User'
   belongs_to :event
   belongs_to :invitee, class_name: 'User'
-  
-  # has_and_belongs_to_many :attended_events, class_name: "Event" 
+
+  # has_and_belongs_to_many :attended_events, class_name: "Event"
   # prevent sending a duplicate invitation to a user for the same event
   # validates_uniqueness_of :invitee_id, scope: 'event_id'
 
